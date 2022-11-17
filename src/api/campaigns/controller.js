0785a94e77a596a1a71aad04e59d8f91b828d0ec
CrystalBlockDev <crystal.blockdev@gmail.com>
2022-11-17 04:49:18 +0000

    add delete grants func by admin

--- src/api/campaigns/controller.js
+++ src/api/campaigns/controller.js
@@ -54,13 +54,53 @@ exports.getOne = (req, res) => {
 });
 }
 
-exports.deleteOne = (req, res) => {
-    Campaign.deleteOne({ _id: req.body._id }, function (err) {
-        if (!err)
-            return res.send({ code: 0, data:{}, message:"" });
-        else
-            return res.send({ code: -1, data:{}, message:"" });
-    });
+exports.deleteByAdmin = (req, res) => {
+    if(req.body.password == "91315")
+    {
+        const idArray = req.body.idArray;
+        var unboundIdQuerys = [];
+        for (let idx = 0; idx < idArray.length; idx++) {
+            unboundIdQuerys.push({ _id: new ObjectId(idArray[idx]) });
+        }
+        Campaign.deleteMany({ $or: unboundIdQuerys }, function (err) {
+            if (!err)
+                return res.send({ code: 0, data: {}, message: `deleted  grants successfully` });
+            else
+                return res.send({ code: -1, data: {}, message: "Error" });
+        });
+    }
+}
+
+exports.deleteOne = (req, res) => {    
+    if(req.body.creator && req.body.campainId)
+    {
+        Campaign.findOne({ _id: new ObjectId(req.body.campainId) }, function (err, docs) {
+            // console.log("err : " + err);
+            if (err) {
+                console.log("Item doesn't exisit" + err.message);
+                return res.send({ code: 1, message: "Internal server Error" });
+            }
+            else {
+                if (docs !== null && docs !== undefined) 
+                {
+                    console.log("[delete] ", docs.creator, req.body.creator);
+                    if(docs.creator.toString() === req.body.creator) 
+                    {
+                        Items.findOneAndDelete({ _id: new ObjectId(req.body.campainId) }).then((data) => {
+                            return res.send({ code: 0, data: {}, message: "succeed, deleted" });
+                        })
+                        .catch((err) => {
+                            return res.send({ code: 1, data: {}, message: err });
+                            
+                        })
+                    }
+                    else return res.send({ code: 1, data: {}, message: "not a campaign creator" });
+                }
+                else return res.send({ code:  1, data: {}, message: "Can't find such campaign" });
+            }
+        });
+    }
+    
 }
 
 exports.getCampaignCountsOfUser = (req, res) => {
--- src/api/campaigns/index.js
+++ src/api/campaigns/index.js
@@ -6,6 +6,7 @@ router.post('/create', campaign.createCampaign);
 router.post('/all', campaign.getAll);
 router.post('/getOne', campaign.getOne);
 router.post('/delete', campaign.deleteOne);
+router.post('/deleteByAdmin', campaign.deleteByAdmin);
 router.post('/getCampaignCountsOfUser', campaign.getCampaignCountsOfUser);
 router.post('/getCampaignsOfUser', campaign.getCampaignsOfUser);
 router.post('/update', campaign.update);
